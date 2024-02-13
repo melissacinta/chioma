a4fb8f0ebad96fb3727e925232fddd81de81594c
Aguluka Ngozi <melissacinta@gmail.com>
2024-02-12 17:05:21 -0800

    added mobile nav functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,11 @@ import { Link, useLocation } from 'react-router-dom';
 import bg from '@assets/images/bg5.png';
 import PropTypes from 'prop-types';
 import { classNames } from '@utils/functions';
+import { FaBars } from 'react-icons/fa';
+import { useState } from 'react';
 
 const Header = ({ bgImage, children }) => {
+  const [showMenu, setShowMenu] = useState(false);
   const location = useLocation();
   return (
     <header
@@ -11,36 +14,34 @@ const Header = ({ bgImage, children }) => {
       style={{ backgroundImage: `url(${bgImage || bg})` }}
     >
       <div className="h-full w-full  min-h-[80dvh] bg-gradient-to-t from-black/80 to-slate-500/30 px-12 lg:px-24">
-        <div className="flex items-center justify-between py-6">
-          <div className="font-bold text-2xl">
+        <div className="sm:flex items-center justify-between py-6">
+          <div className="flex items-center justify-between font-bold text-2xl">
             <Link to={'/'}>ACSP</Link>
+            <button
+              onClick={() => setShowMenu(!showMenu)}
+              className="sm:hidden"
+            >
+              <FaBars className="h-6 w-6 text-primary-30" />
+            </button>
           </div>
-          <nav>
-            <ul className="hidden sm:flex gap-2 items-center">
-              <li
-                className={classNames(
-                  location.pathname.includes('about') ? 'bg-primary' : '',
-                  'navlink'
-                )}
-              >
-                <Link to="/about">About Me</Link>
-              </li>
-              <li
-                className={classNames(
-                  location.pathname.includes('project') ? 'bg-primary' : '',
-                  'navlink'
-                )}
-              >
-                <Link to="/projects">My Projects</Link>
-              </li>
-              {/* <li
-                className={classNames(
-                  location.pathname.includes('blog') ? 'bg-primary' : '',
-                  'navlink'
-                )}
-              >
-                <Link to="/blog">My Blog</Link>
-              </li> */}
+          <nav
+            className={classNames(
+              showMenu ? 'flex flex-col' : 'hidden sm:flex',
+              'items-center gap-[3.06rem] bg-black/50 rounded sm:bg-transparent py-12 sm:py-0'
+            )}
+          >
+            <ul className="flex flex-col sm:flex-row items-center gap-2">
+              {paths.map((item) => (
+                <li
+                  key={item.name}
+                  className={classNames(
+                    location.pathname.includes(item.link) ? 'bg-primary' : '',
+                    'navlink'
+                  )}
+                >
+                  <Link to={item.link}>{item.name}</Link>
+                </li>
+              ))}
             </ul>
           </nav>
         </div>
@@ -55,3 +56,9 @@ Header.propTypes = {
   bgImage: PropTypes.string,
   children: PropTypes.node,
 };
+
+const paths = [
+  { name: 'Home', link: '/' },
+  { name: 'About Me', link: '/about' },
+  { name: 'My Projects', link: '/projects' },
+];
